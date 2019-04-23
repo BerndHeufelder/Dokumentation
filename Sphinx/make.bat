4d46c9f7229f4586f4b4584317bb96321106888e
behe <bernd.heufelder@edu.umit.at>
2019-04-23 14:41:06 +0200

    batch files sperated

--- Sphinx/make.bat
+++ Sphinx/make.bat
@@ -1,27 +1,4 @@
 @ECHO OFF
-
-REM change directory to ...\Sphinx\source
-set dir_source=%~dp0source
-pushd %dir_source%
-set dir_pdf2svg="..\..\SW-Tools\pdf2svg\pdf2svg.exe"
-setlocal enabledelayedexpansion
-
-for /r %%i in (*.pdf) do (
-	REM %%~pi path of current file in loop, %%~ni name of current file in loop, %%~xi file extension (.pdf)
-	
-	echo %%~ni
-	if "%%~ni" == "PMSM_Modell_in_dq" set list=i ii iii iv
-	if "%%~ni" == "Digitale_Regler_V1.2" set list=1 2 3
-	
-	echo !list!
-	for %%u in (!list!) do (
-		%dir_pdf2svg% "%%~pi%%~ni.pdf" "%%~pi%%~ni-%%u.svg" %%u
-	)
-)
-endlocal
-
-cd ..
-
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
@@ -54,3 +31,4 @@ goto end
 
 :end
 popd
+
